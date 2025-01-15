ea8a7af8b9dfffe7544a8e12d21431a65a043aec
Abhishek kumawat <abhishekkumawat1008@gmail.com>
2025-01-15 21:38:01 +0530

    hero section added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     setIsVisible(false);
   };
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-950 font-sans text-lg">
+    <nav className="bg-white border-gray-200 dark:bg-gray-950  font-sans text-lg">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
         <Link
@@ -67,7 +67,7 @@ const Navbar = () => {
           } w-full md:flex md:w-auto md:order-1`}
           id="navbar-user"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-900">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   dark:border-gray-900">
             <li>
               <Link
                 onClick={closeMenu}
